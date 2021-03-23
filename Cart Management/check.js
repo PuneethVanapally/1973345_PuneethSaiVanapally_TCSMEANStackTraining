2a6cc73b700d9b518a57d51fe203550437c9a1ef
Puneeth Sai <puneethvanapalli@gmail.com>
2021-03-23 01:32:21 -0400

    Cart MAnagement

---
+++ Cart Management/check.js
@@ -0,0 +1,15 @@
+var details = JSON.parse(sessionStorage.getItem('details'));
+var table = document.getElementById("myTable");
+var qty = 0;
+for (var i = 0; i < details.length; i++) {
+    var row = table.insertRow(i);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = details[i].name;
+    cell2.innerHTML = details[i].amount;
+    cell3.innerHTML = details[i].quantity + "";
+    qty += +details[i].amount;
+}
+var total = document.getElementById('total');
+total.innerHTML = "Total: " + qty;
